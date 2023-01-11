e678eaa5a92d92841880a76be3007d7a0adfbc7c
Oleksandr FIlippov <110115726+AlexFilp@users.noreply.github.com>
2023-01-11 22:57:05 +0100

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,16 @@
 export const fetchCountries = fetchCountriesFunc;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 function fetchCountriesFunc(name) {
   return fetch(
-    'https://restcountries.com/v3.1/name/uk?fields=name,capital,population,languages,flags'
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
   )
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then(response => {
+      if (!response.ok) {
+        Notify.failure('Oops, there is no country with that name');
+      }
+      return response.json();
+    })
+    .then(data => data)
+    .catch(error => error);
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './fetchCountries';
+
+Notify.init({
+  position: 'center-top',
+  showOnlyTheLastOne: true,
+  cssAnimationStyle: 'zoom',
+  fontSize: '19px',
+  width: '420px',
+});
 
 const DEBOUNCE_DELAY = 300;
 
-fetchCountries();
-
 const refs = {
   formInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
-refs.formInput.addEventListener('input', onInput);
+refs.formInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
-  const input = e.target.value;
-  console.log(input);
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+  const input = e.target.value.trim();
+  fetchCountries(input).then(data => {
+    if (data.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (data.length >= 2 && data.length <= 10) {
+      const countries = data
+        .map(
+          item =>
+            `<li class="country-item"><img src="${item.flags.svg}" alt="flag" width="50" height="40"> ${item.name.common}</li>`
+        )
+        .join('');
+
+      refs.countryList.innerHTML = countries;
+    } else {
+      const country = data[0];
+      refs.countryInfo.innerHTML = `<h1 class="country-title">
+        <img class="country-img" src="${
+          country.flags.svg
+        }" alt="flag" width="120" />
+        ${country.name.common}
+      </h1>
+      <p class="country-text">Capital: <span class="country-span">${
+        country.capital
+      }</span></p>
+      <p class="country-text">Population: <span class="country-span">${
+        country.population
+      }</span></p>
+      <p class="country-text">Languages: <span class="country-span">${Object.keys(
+        country.languages
+      )
+        .map(lang => country.languages[lang])
+        .join(', ')}</span></p>`;
+    }
+  });
 }
