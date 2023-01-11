fb07d325531d70d0893e47dfc8a4e26404093a1c
AlexFilp <110115726+AlexFilp@users.noreply.github.com>
2023-01-11 21:57:36 +0000

    Deploying to gh-pages from @ AlexFilp/goit-js-hw-10@e678eaa5a92d92841880a76be3007d7a0adfbc7c 🚀

--- index.241a551b.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/uk?fields=name,capital,population,languages,flags").then((e=>e.json())).then((e=>console.log(e)));({formInput:document.querySelector("#search-box")}).formInput.addEventListener("input",(function(e){const t=e.target.value;console.log(t)}));
-//# sourceMappingURL=index.241a551b.js.map
